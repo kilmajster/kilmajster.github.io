71f8eacb00d465a2f22002b13334696666ea354b
Łukasz Włódarczyk <lukasz.wlodarczyk@tapptic.com>
2017-06-29 11:06:40 +0200

    revert changes

--- src/io/github/kilmajster/main.js
+++ src/io/github/kilmajster/main.js
@@ -2,7 +2,7 @@ var $ = require("jquery");
 
 $(document).ready(function() {
     $.ajax({
-        url: "https://rest-service.guides.spring.io/greeting"
+        url: "http://rest-service.guides.spring.io/greeting"
     }).then(function(data) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
