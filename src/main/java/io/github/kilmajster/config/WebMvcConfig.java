d0902629c1a49ec226bf894ff425daee9eeb468a
Łukasz Włódarczyk <lukasz.wlodarczyk@tapptic.com>
2017-06-30 12:25:45 +0200

    updated web config

--- src/main/java/io/github/kilmajster/config/WebMvcConfig.java
+++ src/main/java/io/github/kilmajster/config/WebMvcConfig.java
@@ -3,9 +3,11 @@ package io.github.kilmajster.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
