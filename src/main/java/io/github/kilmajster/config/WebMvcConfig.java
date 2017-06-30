a083396f10173ef41e0b68bccbe673d331198ad2
Łukasz Włódarczyk <lukasz.wlodarczyk@tapptic.com>
2017-06-30 12:23:38 +0200

    updated web config

--- src/main/java/io/github/kilmajster/config/WebMvcConfig.java
+++ src/main/java/io/github/kilmajster/config/WebMvcConfig.java
@@ -10,7 +10,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.
         registry.addMapping("https://kilmajster.github.io/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
