bf8d5440afaefd1f34796c85e184f03725e81fe4
Łukasz Włódarczyk <lukasz.wlodarczyk@tapptic.com>
2017-06-30 12:22:23 +0200

    updated web config

--- src/main/java/io/github/kilmajster/config/WebMvcConfig.java
+++ src/main/java/io/github/kilmajster/config/WebMvcConfig.java
@@ -1,13 +1,16 @@
 package io.github.kilmajster.config;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        registry.
         registry.addMapping("https://kilmajster.github.io/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
